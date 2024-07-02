bb11e9f076f6b512fa18a6d4bc43f261b465cf85
jehernandezcode <jhonnhernandez754@gmail.com>
2024-07-02 14:53:24 -0500

    :fix: :refactor: refactor logig number convert by billion

--- api-number-at-letters/obj/Debug/net7.0/api-number-at-letters.AssemblyInfo.cs
+++ api-number-at-letters/obj/Debug/net7.0/api-number-at-letters.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api-number-at-letters")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87e5927938182b8a24ea275e3ee782c050ba4ddc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+325323449c7d0fc10dd6a3200652dba32dfb0ccf")]
 [assembly: System.Reflection.AssemblyProductAttribute("api-number-at-letters")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api-number-at-letters")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
