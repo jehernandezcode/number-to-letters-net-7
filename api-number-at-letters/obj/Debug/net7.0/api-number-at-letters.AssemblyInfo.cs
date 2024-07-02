56f676c500b43dff5b77857b288de574420e3bfd
jehernandezcode <jhonnhernandez754@gmail.com>
2024-07-02 16:14:28 -0500

    :refactor: add files documentation in root project

--- api-number-at-letters/obj/Debug/net7.0/api-number-at-letters.AssemblyInfo.cs
+++ api-number-at-letters/obj/Debug/net7.0/api-number-at-letters.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api-number-at-letters")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+082421263e010ef2e4ea7df69bea0fb8b5bb6d32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a14fc8160ca7e8506b760e22f262e88d987fd3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api-number-at-letters")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api-number-at-letters")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
